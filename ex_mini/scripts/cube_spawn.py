12b78c83ce67ae62d297b215b9528d0d6512d7d0
Bernhard <bernton96@gmail.com>
2021-09-30 10:29:30 +0200

    added general objects for spawning

--- ex_mini/scripts/cube_spawn.py
+++ ex_mini/scripts/cube_spawn.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     delete_model = rospy.ServiceProxy("gazebo/delete_model", DeleteModel)
     spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
 
-    with open("/home/student/catkin_ws/src/hello_ros/urdf/cube.urdf", "r") as f:
+    with open("/home/bernhard/catkin_ws/src/Exercises/ex_mini/urdf/cube.urdf", "r") as f:
         product_xml = f.read()
 
     orient = Quaternion(*tf_conversions.transformations.quaternion_from_euler(0., 0.0, 0.785398))
@@ -29,9 +29,9 @@ if __name__ == '__main__':
         item_pose   =   Pose(Point(x=bin_x, y=bin_y,    z=1),   orient)
         spawn_model(item_name, product_xml, "", item_pose, "world")
 
-    with open("/home/student/catkin_ws/src/hello_ros/urdf/bucket.urdf", "r") as f:
+    with open("/home/bernhard/catkin_ws/src/Exercises/ex_mini/urdf/bucket.urdf", "r") as f:
         product_xml = f.read()
 
-    item_pose   =   Pose(Point(x=0.53, y=-0.23,    z=0.78),   orient)
+    item_pose   =   Pose(Point(x=0.5, y=-0.23,    z=0.78),   orient)
     print("Spawning model:%s", "bucket")
     spawn_model("bucket", product_xml, "", item_pose, "world")
